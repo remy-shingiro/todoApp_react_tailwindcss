557b9f55eab00106d5cc6328fd68ed648de0e3f5
Remy <shingiroremy303@gmail.com>
2026-03-31 14:45:25 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+export default function Todo () {
+
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1>Todo App</h1>
+      </div>
+      
+    </>
+  )
+};
